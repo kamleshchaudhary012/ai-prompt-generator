d754244411e7bf0285025ca0037d9f1229452cf7
kamlesh Chaudhary <chaudharykamlesh185@gmail.com>
2025-04-06 11:47:26 +0530

    Add Render.com deployment configuration

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
+
+# Load initial data (only if needed)
+python manage.py load_initial_data 
